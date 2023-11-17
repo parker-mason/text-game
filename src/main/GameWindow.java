5c31fd4ef198b9337359083dd86aaa9f6f2c82a8
parkermason <120344135+parker-mason@users.noreply.github.com>
2023-11-17 10:15:37 -0700

    Update GameWindow.java

--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -4,10 +4,7 @@ import util.MyFrame;
 import input.GetKey;
 
 public class GameWindow extends MyFrame {
-    protected GetKey getKey;
 
     public GameWindow() {
-        getKey = new GetKey();
-        this.getKey = getKey;
     }
 }
